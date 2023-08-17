8b1c053aea57f720bec8b687af1667c8511731d6
BarSh0 <barshoshani274@gmail.com>
2023-08-17 15:54:52 +0300

    check

--- src/services/connectService/twitter.connect.ts
+++ src/services/connectService/twitter.connect.ts
@@ -20,5 +20,14 @@ export const signInWithTwitter = async () => {
   const result = await signInWithPopup(auth, provider);
   const cred = TwitterAuthProvider.credentialFromResult(result);
   const user = result.user;
-  return await handlePostRequest('twitter/signin', { ...user, ...cred });
+
+  const req = {
+    email: user.providerData[0].email,
+    displayName: user?.displayName,
+    photoURL: user?.photoURL,
+    accessToken: cred?.accessToken,
+    secret: cred?.secret,
+  };
+
+  return await handlePostRequest('twitter/signin', req);
 };
